74aa7fafc7a3119bcebe2ff61331be32f6470222
Szymon Wasik <szymon.wasik@gmail.com>
2025-05-20 10:58:11 +0200

    Add target host name to the list of allowed hosts.

---
+++ webui/frontend/vite.config.js
@@ -0,0 +1,14 @@
+// Vite config for Angular frontend to allow custom hosts
+import { defineConfig } from 'vite';
+import { angular } from '@analogjs/vite-plugin-angular';
+
+export default defineConfig({
+  plugins: [angular()],
+  server: {
+    allowedHosts: [
+      'swasik-vsearch-test-usearch.germanywestcentral.cloudapp.azure.com',
+      'localhost',
+      '127.0.0.1'
+    ]
+  }
+});
